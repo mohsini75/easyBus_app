e0b241b4a43912b786fcd2483411503b3ad4ca94
Muhammad Umair Bin Sajid <sardarmuhammadumair786@gmail.com>
2021-12-18 23:18:20 +0500

    save login user state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 // @dart = 2.9
 import 'dart:io' show Platform;
 import 'package:demo/services/challan_pdf.dart';
+import 'package:demo/store_user_role.dart';
 import 'package:demo/users/admin/admin_auth.dart';
 import 'package:demo/users/driver/bottom_tab_screen.dart';
 import 'package:demo/users/driver/driver_auth.dart';
@@ -15,10 +16,12 @@ import 'package:demo/users/student/my_transactions_chart.dart';
 import 'package:demo/users/student/student_auth.dart';
 import 'package:demo/web_app/HomePage.dart';
 import 'package:demo/welcome_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'users/admin/dashboard.dart';
 import 'users/driver/dashboard.dart';
@@ -34,15 +37,43 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Widget selectedWidget;
+
+  @override
+  initState() async {
+    super.initState();
+    if (Platform.isWindows) {
+      selectedWidget = const MyHomePage();
+    } else {
+      if (FirebaseAuth.instance.currentUser != null) {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+
+        // await prefs.setString('currentUserRole', role);
+        String currentUser = prefs.getString('currentUserRole');
+        if (currentUser == 'driver') {
+          //  selectedWidget = driverBottomSheet
+        } else if (currentUser == 'Student') {}
+
+        // String currentLogInUser = getUserRole() as String;
+      } else {
+        selectedWidget = WelcomeScreen();
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Parent ',
       debugShowCheckedModeBanner: false,
       //initialRoute: WelcomeScreen.routeName,
-      home: Platform.isWindows ? MyHomePage() : WelcomeScreen(),
+      home: selectedWidget,
       routes: {
         WelcomeScreen.routeName: (context) => WelcomeScreen(),
         StudentAuth.routeName: (context) => StudentAuth(),
